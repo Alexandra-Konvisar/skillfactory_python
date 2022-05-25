ec35b65d0cf3f8bb31da9fdd05033ec5c35ccc7e
Alexandra-Konvisar <46960334+Alexandra-Konvisar@users.noreply.github.com>
2022-05-25 17:37:25 +0400

    18.6 project

---
+++ config.py
@@ -0,0 +1,11 @@
+
+TOKEN = '5303282106:AAFZHSN-HedN8lWwdTnsa0pj8Chbjg6E5ZE'
+keys = {
+    'Юань': 'CNY',
+    'Йена': 'JPY',
+    'Доллар': 'USD',
+    'Евро': 'EUR',
+    'Франк': 'CHF',
+    'Лира': 'TRY',
+    'Рубль': 'RUB',
+}
